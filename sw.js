e85acc3285b87ca78e551e7a0816484b1df0bccb
t1mm1et33 <t1mm1e@wearehackerone.com>
2024-12-11 22:26:23 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-console.log('This is a test script executed via sw.js');
+self.addEventListener('install', (event) => {
+    console.log('Test Service Worker Installed');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Test Service Worker Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Test Service Worker Fetch Intercept:', event.request.url);
+});
